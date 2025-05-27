71b1fe54b89e37b415c2f1c362a452829527800e
Brenda <brenvaltessy@yahoo.com>
2025-05-27 13:07:27 +0100

    created api branch and added a predict endpoint

---
+++ employee.sql
@@ -0,0 +1,13 @@
+create database my_db;
+create schema my_schema
+CREATE TABLE (Employee
+id INT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+department,
+position,
+salary,
+);
+
+select * from employee;
+
+Alter table Employee;
